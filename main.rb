a8b78ea398c85dba17f80876ef280dc021ff8374
Bridges369 <75232236+Bridges369@users.noreply.github.com>
2023-01-21 00:05:50 -0300

    now it's allow to add prefix and sufix for each models

--- main.rb
+++ main.rb
@@ -14,8 +14,12 @@ class Parse
       # The method below of this don't return the string, therefore the gsub! method will be used in a variable tmp
       tmp = text.chomp
       # Replace tmp according to the each replacements hashes
-      hash[1].each { |k, v| tmp.gsub!(k, v) }
-      collect[i] = tmp
+      hash[1].each do |k, v| 
+        if (k != :prefix or k == :sufix) and (k == :prefix or k != :sufix)
+          tmp.gsub!(k, v)
+        end
+      end
+      collect[i] = (hash[1][:prefix] or "") + tmp + (hash[1][:sufix] or "")
     end
     return collect
   end
